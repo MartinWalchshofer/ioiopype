c6948debe46f8cf652fa0525776a50f4f9755865
Martin Walchshofer <mwalchshofer@gmx.net>
2024-03-14 19:19:59 +0100

    updated unicorn example

--- examples/example_unicorn_pwelch.py
+++ examples/example_unicorn_pwelch.py
@@ -34,11 +34,22 @@ device = ioio.Unicorn(discovered_devices[selectedId])
 buf = ioio.Buffer(device.NumberOfEEGChannels, 4 * device.SamplingRateInHz, 4 * device.SamplingRateInHz - 25)
 pw = ioio.PWelch(device.SamplingRateInHz)
 fp = ioio.FramePlot(samplingRate=4)
+hp = ioio.ButterworthFilter(ioio.FilterType.Highpass, device.SamplingRateInHz, 2, [2])
+lp = ioio.ButterworthFilter(ioio.FilterType.Lowpass, device.SamplingRateInHz, 4, [30])
+n50 = ioio.ButterworthFilter(ioio.FilterType.Notch, device.SamplingRateInHz, 4, [48, 52])
+n60 = ioio.ButterworthFilter(ioio.FilterType.Notch, device.SamplingRateInHz, 4, [58, 62])
+sp = ioio.SamplePlot(device.NumberOfEEGChannels, device.SamplingRateInHz, 6, 100)
 
 #build ioiopype
 device.connect(0, buf.InputStreams[0])
 buf.connect(0, pw.InputStreams[0])
 pw.connect(0, fp.InputStreams[0])
+
+device.connect(0, n50.InputStreams[0])
+n50.connect(0, n60.InputStreams[0])
+n60.connect(0, hp.InputStreams[0])
+hp.connect(0, lp.InputStreams[0])
+lp.connect(0, sp.InputStreams[0])
 
 input('Press any key to terminate the application\n')
 
@@ -46,6 +57,12 @@ input('Press any key to terminate the application\n')
 device.disconnect(0, buf.InputStreams[0])
 buf.disconnect(0, pw.InputStreams[0])
 pw.disconnect(0, fp.InputStreams[0])
+
+device.disconnect(0, n50.InputStreams[0])
+n50.disconnect(0, n60.InputStreams[0])
+n60.disconnect(0, hp.InputStreams[0])
+hp.disconnect(0, lp.InputStreams[0])
+lp.disconnect(0, sp.InputStreams[0])
 
 #close device
 del device
