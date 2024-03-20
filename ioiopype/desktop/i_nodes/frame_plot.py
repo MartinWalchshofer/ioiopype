9b59630b1bff1e28d4b0fef1752ef90234e9590a
Martin Walchshofer <mwalchshofer@gmx.net>
2024-03-20 08:39:11 +0100

    improved plots

--- ioiopype/desktop/i_nodes/frame_plot.py
+++ ioiopype/desktop/i_nodes/frame_plot.py
@@ -1,47 +1,69 @@
 from ...pattern.i_node import INode
 from ...pattern.i_stream import IStream
 from ...pattern.stream_info import StreamInfo
+from PyQt6 import QtCore, QtGui
 import pyqtgraph as pg
 import numpy as np
+import time
 
-class FramePlot(INode):
+class QTFramePlot(QtCore.QObject):
+    updateSignal = QtCore.pyqtSignal(np.ndarray)
+
     def __init__(self, samplingRate=1, displayedAmplitude=[]):
-        super().__init__()
-        self.add_i_stream(IStream(StreamInfo(0, 'data', StreamInfo.Datatype.Frame)))
+        super(QtCore.QObject, self).__init__()
+
         self.samplingRate = samplingRate
         self.plotWidget = pg.plot(title="frame plot")
-        if len(displayedAmplitude) is 2:
+        if len(displayedAmplitude) == 2:
             self.plotWidget.setYRange(displayedAmplitude[0], displayedAmplitude[1], 0)
-        elif len(displayedAmplitude) is 1:
+        elif len(displayedAmplitude) == 1:
             self.plotWidget.setYRange(-displayedAmplitude[0], displayedAmplitude[0], 0)
+
         self.x = None
-        self.y = None
         self.numberOfChannels = 0
         self.items = []
-        self.timer = pg.QtCore.QTimer()
-        self.timer.timeout.connect(self.update_plot)
-        self.timer.start(round(1000/25))
 
-    def __del__(self):
-        super().__del__()
-        self.timer.stop()
+        self.updateSignal.connect(self.update_plot)
+    
+    def update_plot(self, y):
+        if y is not None:
+            if self.x is None or self.x.shape[0] != y.shape[0]:
+                self.x = np.linspace(1, y.shape[0], y.shape[0])
+                self.x = np.divide(self.x,self.samplingRate)
 
-    def update_plot(self):
-        if self.y is not None:
-            self.numberOfChannels = self.y.shape[1]
+            self.numberOfChannels = y.shape[1]
             if len(self.items) is not self.numberOfChannels:
                 for i in range(0, self.numberOfChannels):
                     self.items.append(pg.PlotCurveItem())
                     self.plotWidget.addItem(self.items[i])
             for i in range(0, self.numberOfChannels):
-                self.items[i].setData(x=self.x, y= self.y[:,i])
+                self.items[i].setData(x=self.x, y= y[:,i])
+
+class FramePlot(INode):
+    UpdateRateHz = 20
+
+    def __init__(self, samplingRate=1, displayedAmplitude=[]):
+        super().__init__()
+        self.add_i_stream(IStream(StreamInfo(0, 'data', StreamInfo.Datatype.Frame)))
+        
+        self.__timestamp = 0
+        self.__dT = 0
+        self.dTS = 1/self.UpdateRateHz
+        self.qto = QTFramePlot(samplingRate, displayedAmplitude)
+
+    def __del__(self):
+        super().__del__()
 
     def update(self):
         data = None
         if self.InputStreams[0].DataCount > 0:
             data = self.InputStreams[0].read()
         if data is not None:
-            if self.x is None or self.x.shape[0] != data.shape[0]:
-                self.x = np.linspace(1, data.shape[0], data.shape[0])
-                self.x = np.divide(self.x,self.samplingRate)
+            
             self.y =  data
+        timestamp = time.time()
+        self.__dT += timestamp - self.__timestamp
+        self.__timestamp = timestamp
+        if self.__dT >= self.dTS:
+            self.qto.updateSignal.emit(data)
+            self.__dT = 0
--- ioiopype/desktop/i_nodes/sample_plot.py
+++ ioiopype/desktop/i_nodes/sample_plot.py
@@ -2,28 +2,24 @@ from ...pattern.i_node import INode
 from ...pattern.i_stream import IStream
 from ...pattern.stream_info import StreamInfo
 from ...common.utilities.overriding_buffer import OverridingBuffer
+from enum import Enum
+from PyQt6 import QtCore
 import pyqtgraph as pg
 import numpy as np
-from enum import Enum
+import time
 
-class SamplePlot(INode):
-    class DisplayMode(Enum):
-        Overriding = 1
-        Continous = 2
+class QTSamplePlot(QtCore.QObject):
+    updateSignal = QtCore.pyqtSignal(OverridingBuffer)
 
-    def __init__(self, numberOfChannels, samplingRate, displayedTimeRangeS, amplitude, displayMode=DisplayMode.Overriding):
-        super().__init__()
-        self.add_i_stream(IStream(StreamInfo(0, 'in', StreamInfo.Datatype.Sample)))
+    def __init__(self, numberOfChannels, samplingRate, displayedTimeRangeSamples, amplitude):
+        super(QtCore.QObject, self).__init__()
+
         self.numberOfChannels = numberOfChannels
         self.samplingRate = samplingRate
-        self.displayedTimeRangeS = displayedTimeRangeS
-        self.displayedTimeRangeSamples = samplingRate * displayedTimeRangeS
-        self.x = np.linspace(1, self.displayedTimeRangeSamples, self.displayedTimeRangeSamples)
-        self.x = np.divide(self.x,self.samplingRate)
-        if displayMode is self.DisplayMode.Overriding:
-            self.buffer = OverridingBuffer(self.displayedTimeRangeSamples , self.numberOfChannels, OverridingBuffer.OutputMode.NotAligned)
-        else:
-            self.buffer = OverridingBuffer(self.displayedTimeRangeSamples , self.numberOfChannels, OverridingBuffer.OutputMode.Aligned)
+        
+        self.x = np.linspace(1, displayedTimeRangeSamples, displayedTimeRangeSamples)
+        self.x = np.divide(self.x,samplingRate)
+
         self.offsets = []
         for i in range(0, self.numberOfChannels):
             if i % 2 == 0:
@@ -45,24 +41,49 @@ class SamplePlot(INode):
         else:
             minMaxAmplitude = (amplitude/2)+((self.numberOfChannels/2)-0.5)*amplitude
             self.plotWidget.setYRange(-1 * minMaxAmplitude, minMaxAmplitude, 0)
-
-        self.timer = pg.QtCore.QTimer()
-        self.timer.timeout.connect(self.update_plot)
-        self.timer.start(round(1000/25))
-
-    def __del__(self):
-        super().__del__()
-        self.timer.stop()
-
-    def update_plot(self):
-        data = self.buffer.getFrame()
+        
+        self.updateSignal.connect(self.update_plot)
+    
+    def update_plot(self, buffer):
+        data = buffer.getFrame()
         for i in range(0, self.numberOfChannels):
             self.items[i].setData(x=self.x, y= data[:,i] + self.offsets[i])
 
+class SamplePlot(INode):
+    UpdateRateHz = 20
+
+    class DisplayMode(Enum):
+        Overriding = 1
+        Continous = 2
+
+    def __init__(self, numberOfChannels, samplingRate, displayedTimeRangeS, amplitude, displayMode=DisplayMode.Overriding):
+        super().__init__()
+        self.add_i_stream(IStream(StreamInfo(0, 'in', StreamInfo.Datatype.Sample)))
+        
+        displayedTimeRangeSamples = samplingRate * displayedTimeRangeS
+        if displayMode is self.DisplayMode.Overriding:
+            self.buffer = OverridingBuffer(displayedTimeRangeSamples , numberOfChannels, OverridingBuffer.OutputMode.NotAligned)
+        else:
+            self.buffer = OverridingBuffer(displayedTimeRangeSamples , numberOfChannels, OverridingBuffer.OutputMode.Aligned)
+
+        self.__timestamp = 0
+        self.__dT = 0
+        self.dTS = 1/self.UpdateRateHz
+        self.qto = QTSamplePlot(numberOfChannels, samplingRate, displayedTimeRangeSamples, amplitude)
+        
+    def __del__(self):
+        super().__del__()
+
     def update(self):
-        data = None
+        data = None        
         if self.InputStreams[0].DataCount > 0:
             data = self.InputStreams[0].read()
         if data is not None:
             for row in data:
                 self.buffer.setData(data)
+        timestamp = time.time()
+        self.__dT += timestamp - self.__timestamp
+        self.__timestamp = timestamp
+        if self.__dT >= self.dTS:
+            self.qto.updateSignal.emit(self.buffer)
+            self.__dT = 0
