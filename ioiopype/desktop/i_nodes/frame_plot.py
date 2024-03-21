475722b85d1097e1dd91808559c6949846de7f90
Martin Walchshofer <mwalchshofer@gmx.net>
2024-03-21 23:09:43 +0100

    Update frame_plot.py

--- ioiopype/desktop/i_nodes/frame_plot.py
+++ ioiopype/desktop/i_nodes/frame_plot.py
@@ -6,42 +6,42 @@ import pyqtgraph as pg
 import numpy as np
 import time
 
-class QTFramePlot(QObject):
-    updateSignal = Signal(np.ndarray)
-
-    def __init__(self, samplingRate=1, displayedAmplitude=[]):
-        super(QTFramePlot, self).__init__()
-
-        self.samplingRate = samplingRate
-        self.plotWidget = pg.plot(title="frame plot")
-        if len(displayedAmplitude) == 2:
-            self.plotWidget.setYRange(displayedAmplitude[0], displayedAmplitude[1], 0)
-        elif len(displayedAmplitude) == 1:
-            self.plotWidget.setYRange(-displayedAmplitude[0], displayedAmplitude[0], 0)
-
-        self.x = None
-        self.numberOfChannels = 0
-        self.items = []
-
-        self.updateSignal.connect(self.update_plot)
-
-    @Slot(np.ndarray)
-    def update_plot(self, y):
-        if y is not None:
-            if self.x is None or self.x.shape[0] != y.shape[0]:
-                self.x = np.linspace(1, y.shape[0], y.shape[0])
-                self.x = np.divide(self.x,self.samplingRate)
-
-            self.numberOfChannels = y.shape[1]
-            if len(self.items) is not self.numberOfChannels:
-                for i in range(0, self.numberOfChannels):
-                    self.items.append(pg.PlotCurveItem())
-                    self.plotWidget.addItem(self.items[i])
-            for i in range(0, self.numberOfChannels):
-                self.items[i].setData(x=self.x, y= y[:,i])
-
 class FramePlot(INode):
     UpdateRateHz = 20
+
+    class QTFramePlot(QObject):
+        updateSignal = Signal(np.ndarray)
+
+        def __init__(self, samplingRate=1, displayedAmplitude=[]):
+            super(FramePlot.QTFramePlot, self).__init__()
+
+            self.samplingRate = samplingRate
+            self.plotWidget = pg.plot(title="frame plot")
+            if len(displayedAmplitude) == 2:
+                self.plotWidget.setYRange(displayedAmplitude[0], displayedAmplitude[1], 0)
+            elif len(displayedAmplitude) == 1:
+                self.plotWidget.setYRange(-displayedAmplitude[0], displayedAmplitude[0], 0)
+
+            self.x = None
+            self.numberOfChannels = 0
+            self.items = []
+
+            self.updateSignal.connect(self.update_plot)
+
+        @Slot(np.ndarray)
+        def update_plot(self, y):
+            if y is not None:
+                if self.x is None or self.x.shape[0] != y.shape[0]:
+                    self.x = np.linspace(1, y.shape[0], y.shape[0])
+                    self.x = np.divide(self.x,self.samplingRate)
+
+                self.numberOfChannels = y.shape[1]
+                if len(self.items) is not self.numberOfChannels:
+                    for i in range(0, self.numberOfChannels):
+                        self.items.append(pg.PlotCurveItem())
+                        self.plotWidget.addItem(self.items[i])
+                for i in range(0, self.numberOfChannels):
+                    self.items[i].setData(x=self.x, y= y[:,i])
 
     def __init__(self, samplingRate=1, displayedAmplitude=[]):
         super().__init__()
@@ -50,7 +50,7 @@ class FramePlot(INode):
         self.__timestamp = 0
         self.__dT = 0
         self.dTS = 1/self.UpdateRateHz
-        self.qto = QTFramePlot(samplingRate, displayedAmplitude)
+        self.qto = FramePlot.QTFramePlot(samplingRate, displayedAmplitude)
 
     def __del__(self):
         super().__del__()
