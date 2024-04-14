854ebf2e93e1220e6b0391cc346b33fc0fd00117
Martin Walchshofer <mwalchshofer@gmx.net>
2024-04-14 12:13:46 +0200

    Update example_matrix_multiply.py

--- examples/example_matrix_multiply.py
+++ examples/example_matrix_multiply.py
@@ -8,28 +8,28 @@ import ioiopype as ioio
 import numpy as np
 
 samplingRate = 100
-m1 = ioio.Constant(samplingRate, np.random.rand(3,3))
+v = ioio.NoiseGenerator(samplingRate, 3, 0, 1)
 m2 = ioio.Constant(samplingRate, np.random.rand(3,3))
 m3 = ioio.Constant(samplingRate, np.random.rand(3,3))
 m = ioio.MatrixMultiply(3)
 c = ioio.ConsoleLog()
 
-m1.connect(0, m.InputStreams[0])
+v.connect(0, m.InputStreams[0])
 m2.connect(0, m.InputStreams[1])
 m3.connect(0, m.InputStreams[2])
 m.connect(0, c.InputStreams[0])
 
-m1.start()
+v.start()
 m2.start()
 m3.start()
 
 input('Press ENTER to terminate the application\n')
 
-m1.stop()
+v.stop()
 m2.stop()
 m3.stop()
 
-m1.disconnect(0, m.InputStreams[0])
+v.disconnect(0, m.InputStreams[0])
 m2.disconnect(0, m.InputStreams[1])
 m3.disconnect(0, m.InputStreams[2])
 m.disconnect(0, c.InputStreams[0])
